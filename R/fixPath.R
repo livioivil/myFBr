379e9ee4cb99c1bdba518662fd2b1cd609119633
livio <livio@stat.unipd.it>
2016-01-07 18:59:57 +0100

    .fixPercorso migliorata

--- R/fixPath.R
+++ R/fixPath.R
@@ -1,35 +1,34 @@
 ##' @description Validity check (and fix) for files path
-##'
 ##' @title fixPath
-##' @param file.path
-##' @return correct file.path
+##' @param file.path path
+##' @param silent FALSE
+##' @return correct file.path 
 ##' @export
-##' 
 ##' @author Livio Finos
 
-fixPath <- function(percorso,silent=FALSE){
-  dirtemp=dir(percorso)
+fixPath <- function(file.path,silent=FALSE){
+  dirtemp=dir(file.path)
   changed=FALSE
   if("__MACOSX"%in%dirtemp){    
-    percorso=paste(percorso,sep="/",setdiff(dirtemp,"__MACOSX")[1])
-    dirtemp=dir(percorso)
+    file.path=paste(file.path,sep="/",setdiff(dirtemp,"__MACOSX")[1])
+    dirtemp=dir(file.path)
     changed=TRUE
   }
   continue=TRUE
   while((!("index.htm"%in%dirtemp)) && continue){
     changed=TRUE
     if(!is.null(dirtemp[1])){
-      percorso=paste(percorso,sep="/",dirtemp[1])
-      dirtemp=dir(percorso)
+      file.path=paste(file.path,sep="/",dirtemp[1])
+      dirtemp=dir(file.path)
     } else {
       continue=FALSE
       if(!silent) warning("The path does not contain the correct files")
-      percorso=NA
+      file.path=NA
       changed=FALSE
     }
   }
   if(changed & (!silent) ) 
     warning("The path has been modified to:")
   
-  percorso
+  file.path
 }
